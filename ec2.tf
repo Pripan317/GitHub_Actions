590baa24148dd9944040dbe75928d44e37c5d178
Prince <118051446+Pripan317@users.noreply.github.com>
2023-07-24 12:34:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "awsec2demo" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
+key_name      = "prince"
 tags = {
     Name = "NI_quickcloudpocs"
   }
